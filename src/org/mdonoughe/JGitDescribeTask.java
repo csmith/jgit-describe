a3af934b2c198dfe36a530395d43817ae1187bc7
Shane Mc Cormack <shanemcc@gmail.com>
2012-01-13 13:17:38 +0000

    Oops, change the right : to ;

--- src/org/mdonoughe/JGitDescribeTask.java
+++ src/org/mdonoughe/JGitDescribeTask.java
@@ -81,7 +81,7 @@ public class JGitDescribeTask extends Task {
         if (!file.isDirectory()) {
             try {
                 final String content = fileAsString(file);
-                final String[] bits = content.split(";", 2);
+                final String[] bits = content.split(":", 2);
                 if (bits.length > 1) {
                     return getGitDir(new File(bits[1].trim()));
                 }
@@ -158,7 +158,7 @@ public class JGitDescribeTask extends Task {
 
         if (subdir != null) {
             final String parent = dir.getParent() + File.separator;
-            for (String sd : subdir.split(":")) {
+            for (String sd : subdir.split(";")) {
                 sd = sd.replaceFirst("^" + Pattern.quote(parent), "");
                 if (!new File(parent + sd).exists()) {
                     throw new BuildException("'"+sd+"' does not appear to be a subdir of this repo.");
